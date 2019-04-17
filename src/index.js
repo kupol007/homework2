640bc367290b2b81908d0cc06dcf1a41748a331f
kupol007 <kupol007@mail.ru>
2019-04-17 20:07:36 +0300

    homework2

--- src/index.js
+++ src/index.js
@@ -1,77 +1,91 @@
 import "babel-polyfill";
 import Chart from "chart.js";
 
-const currencyURL = "www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
-// const meteoURL = "/xml.meteoservice.ru/export/gismeteo/point/140.xml";
+const currencyURL = "/xml.meteoservice.ru/export/gismeteo/point/140.xml";
 
-async function loadCurrency() {
-  const response = await fetch(currencyURL);
-  const xmlTest = await response.text();
-  const parser = new DOMParser();
-  const currencyData = parser.parseFromString(xmlTest, "text/xml");
-  // <Cube currency="USD" rate="1.1321" />
-  const rates = currencyData.querySelectorAll("Cube[currency][rate]");
-  const result = Object.create(null);
-  for (let i = 0; i < rates.length; i++) {
-    const rateTag = rates.item(i);
-    const rate = rateTag.getAttribute("rate");
-    const currency = rateTag.getAttribute("currency");
-    result[currency] = rate;
-  }
-  result["EUR"] = 1;
-  // result["RANDOM"] = 1 + Math.random();
-  return result;
+async function loadWeather() {
+    const response = await fetch(currencyURL);
+    const xmlTest = await response.text();
+    const parser = new DOMParser();
+    const currencyData = parser.parseFromString(xmlTest, "text/xml");
+    const temperatures = currencyData.querySelectorAll("TEMPERATURE[max][min]");
+    const times = currencyData.querySelectorAll("FORECAST[hour]");
+    const heats = currencyData.querySelectorAll("HEAT[max][min]");
+    var result = [];
+
+    for (let i = 0; i < temperatures.length; i++) {
+        const temperatureTag = temperatures.item(i);
+        const timeTag = times.item(i);
+        const heatTag = heats.item(i);
+        const time = Number(timeTag.getAttribute("hour"));
+        const temperatureMax = Number(temperatureTag.getAttribute("max"));
+        const temperatureMin = Number(temperatureTag.getAttribute("min"));
+        const heatMin = Number(heatTag.getAttribute("min"));
+        const heatMax = Number(heatTag.getAttribute("max"));
+        result[i] = { time, temperatureMax, temperatureMin, heatMin, heatMax };
+    }
+
+    return result;
 }
 
-function normalizeDataByCurrency(data, currency) {
-  const result = Object.create(null);
-  const value = data[currency];
-  for (const key of Object.keys(data)) {
-    result[key] = value / data[key];
-  }
-  return result;
+function normalizeDataByTemperature(data, vid) {
+    const result = [];
+
+    for (var i = 0; i < data.length; i++) {
+        result[i] = data[i][vid];
+    }
+
+    return result;
 }
 
 const buttonBuild = document.getElementById("btn");
 const canvasCtx = document.getElementById("out").getContext("2d");
 buttonBuild.addEventListener("click", async function() {
-  const currencyData = await loadCurrency();
-  const normalData = normalizeDataByCurrency(currencyData, "RUB");
-  const keys = Object.keys(normalData).sort((k1, k2) =>
-    compare(normalData[k1], normalData[k2])
-  );
-  const plotData = keys.map(key => normalData[key]);
+    const currencyData = await loadWeather();
+    const temperatureMaxData = normalizeDataByTemperature(currencyData, "temperatureMax");
+    const keys = normalizeDataByTemperature(currencyData, "time");
+    const temperatureMinData = normalizeDataByTemperature(currencyData, "temperatureMin");
+    const heatMinData = normalizeDataByTemperature(currencyData, "heatMin");
+    const heatMaxData = normalizeDataByTemperature(currencyData, "heatMax");
 
-  const chartConfig = {
-    type: "line",
+    const chartConfig = {
+        type: "line",
 
-    data: {
-      labels: keys,
-      datasets: [
-        {
-          label: "Стоимость валюты в рублях",
-          backgroundColor: "rgb(255, 20, 20)",
-          borderColor: "rgb(180, 0, 0)",
-          data: plotData
+        data: {
+            labels: keys,
+            datasets: [{
+                    label: "Максимальная температура воздуха",
+                    borderColor: "#0F0",
+                    borderColor: "#0F0",
+                    data: temperatureMaxData
+                },
+                {
+                    label: "Минимальная температура воздуха",
+                    borderColor: "#00F",
+                    data: temperatureMinData
+                },
+                {
+                    label: "Максимальная температура коморта",
+                    borderColor: "#F00",
+                    data: heatMaxData
+                },
+                {
+                    label: "Минимальная температура коморта",
+                    borderColor: "#FF7F00",
+                    data: heatMinData
+                }
+            ]
         }
-      ]
+    };
+
+    if (window.chart) {
+        chart.data.labels = chartConfig.data.labels;
+        chart.data.datasets[0].data = chartConfig.data.datasets[0].data;
+        chart.update({
+            duration: 800,
+            easing: "easeOutBounce"
+        });
+    } else {
+        window.chart = new Chart(canvasCtx, chartConfig);
     }
-  };
-
-  if (window.chart) {
-    chart.data.labels = chartConfig.data.labels;
-    chart.data.datasets[0].data = chartConfig.data.datasets[0].data;
-    chart.update({
-      duration: 800,
-      easing: "easeOutBounce"
-    });
-  } else {
-    window.chart = new Chart(canvasCtx, chartConfig);
-  }
 });
-
-function compare(a, b) {
-  if (a > b) return 1;
-  if (a < b) return -1;
-  return 0;
-}
